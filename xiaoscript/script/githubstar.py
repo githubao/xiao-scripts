453d949325efd932e7a0b0f353e191019117e6a0
baoqiang <baoqiang@bytedance.com>
2018-07-20 14:43:34 +0800

    github stars
    Change-Id: I45f4ceb0fabd817b1d80a98e31c9673a0cf3e794

--- xiaoscript/script/githubstar.py
+++ xiaoscript/script/githubstar.py
@@ -8,32 +8,39 @@
 @time: 2018/2/13 11:13
 """
 
-import requests
-from collections import OrderedDict, defaultdict
+from __future__ import print_function, with_statement
+
 import json
-import sys
-import matplotlib.pyplot as plt
-import time
 import random
-import platform
-from github import Github
+import sys
+import time
+from collections import OrderedDict, defaultdict
 
-plt.rcParams['font.sans-serif'] = ['SimHei']
+import requests
 
-out_file = '/mnt/home/baoqiang/github.json'
-ps_file = '/mnt/home/baoqiang/1.txt'
+try:
+    from github import Github
+    import matplotlib.pyplot as plt
+except:
+    pass
 
-if 'Windows' in platform.platform():
-    ps_file = 'C:\\Users\\xiaobao\\Desktop\\1.txt'
-    out_file = 'C:\\Users\\xiaobao\\Desktop\\github.json'
+# plt.rcParams['font.sans-serif'] = ['SimHei']
+
+ps_file = '/home/baoqiang/data/ps.txt'
+out_file = '../data/github.json'
+
+# if 'Windows' in platform.platform():
+#     ps_file = 'C:\\Users\\xiaobao\\Desktop\\1.txt'
+#     out_file = 'C:\\Users\\xiaobao\\Desktop\\github.json'
 
 out_file2 = 'C:\\Users\\xiaobao\\Desktop\\github-lang.txt'
 
-focus_keys = ['id', 'full_name', 'description', 'language', 'stargazers_count', 'forks_count', 'url', 'git_url']
-
-start = 1000
+start = 500
 step = 50
-end = 7850
+end = 10000
+
+focus_keys = ['id', 'url', 'name', 'description', 'language',
+              'forks', 'stars', 'created_at', 'updated_at', 'full_name']
 
 
 def run():
@@ -56,7 +63,9 @@ def run():
         dic_list.append(json_data)
 
         # 后续的请求
-        pg_num = get_pg_num(json_data['total_count'])
+        total = json_data['total_count']
+        pg_num = get_pg_num(total)
+        print('start: {}, end: {}, total: {}'.format(i, i + step, total))
         if pg_num > 2:
             for j in range(2, pg_num):
                 json_data2 = req(url_fmt.format(i, i + step, j), auth)
@@ -71,6 +80,7 @@ def run():
     result_dic = {}
     for json_data in dic_list:
         for item in json_data['items']:
+            _hack(item)
             dic = OrderedDict()
             for key in focus_keys:
                 dic[key] = item[key]
@@ -78,7 +88,7 @@ def run():
             result_dic[dic['id']] = dic
 
     # 排序
-    sorted_dic = sorted(result_dic.items(), key=lambda x: x[1]['stargazers_count'], reverse=True)
+    sorted_dic = sorted(result_dic.items(), key=lambda x: x[1]['stars'] + x[1]['forks'], reverse=True)
 
     with open(out_file, 'a', encoding='utf-8') as fw:
         for item in sorted_dic:
@@ -108,12 +118,17 @@ url_fmt = 'https://api.github.com/search/repositories?q=stars:{}..{}&sort=stars&
 
 
 def get_auth():
-    # with open(ps_file, 'r', encoding='utf-8') as f:
-    with open(ps_file, 'r', encoding='utf-8') as f:
+    with open(ps_file, 'r') as f:
         for line in f:
             attr = line.strip().split(' ')
 
             return attr[0], attr[1]
+    # return 'mailbaoqiang@gmail.com', raw_input('password: ')
+
+
+def _hack(dic):
+    dic['url'] = dic['html_url']
+    dic['stars'] = dic['stargazers_count']
 
 
 def count_lang():
@@ -192,16 +207,16 @@ def update_desc():
 def process_repo():
     # url = 'https://api.github.com/user/repos'
     url = 'https://api.github.com/repos/githubao/test-aaa'
-    res = requests.delete(url,auth=get_auth())
+    res = requests.delete(url, auth=get_auth())
     print(res)
 
 
 def main():
-    # run()
+    run()
     # count_lang()
     # github_api()
     # update_desc()
-    process_repo()
+    # process_repo()
 
 
 if __name__ == '__main__':
